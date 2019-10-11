e8df0c94a76c3bcac40c7f480540830dcf4c9482
Pratik <pratik.pawar@routemobile.com>
2019-10-11 13:08:14 +0530

    auth redirect url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   auth0: {
     clientID: 'NtCkn5QwFGk1rjv9mUsoXmTquUA5MoKw',
     domain: 'auth-prtkpwr.auth0.com',
-    redirectUri: 'https://f59ebb54.ngrok.io/',
-    logoutUrl: 'https://f59ebb54.ngrok.io/'
+    redirectUri: 'https://audio-player-b816b.firebaseapp.com/',
+    logoutUrl: 'https://audio-player-b816b.firebaseapp.com/'
   }
 };
